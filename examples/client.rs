dc5db8e8e1238c311fd6cbbdb7a9937772d3e570
Salmaan Saeed <salsaeed999@gmail.com>
2023-05-29 19:05:15 -0400

    removed import

--- examples/client.rs
+++ examples/client.rs
@@ -1,11 +1,10 @@
 use mini_redis::{client, Result};
-use mini_redis::client::Client;
 use bytes::Bytes;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     // Open a connection to the mini-redis address
-    let mut client: Client = client::connect("127.0.0.1:6379").await?;
+    let mut client: client::Client = client::connect("127.0.0.1:6379").await?;
 
     // Set the key "hello" with value "world"
     client.set("hello", "world".into()).await?;
