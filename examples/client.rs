7eb8a2715144cc93d490e5a55ea6cfd9137d66ec
Salmaan Saeed <salsaeed999@gmail.com>
2023-05-30 01:09:18 -0400

    The client gets a message output if the client successfully sent a message to the server.

--- examples/client.rs
+++ examples/client.rs
@@ -6,15 +6,16 @@ async fn main() {
     // Open a connection to the address
     let client: Result<TcpStream, Error> = TcpStream::connect("127.0.0.1:6379").await;
 
-    let msg = b"Hello";
+    // The message as an array of 8 bit unsinged integers
+    let msg: &[u8] = b"Hello";
 
     match client {
         Ok(stream) => {
-            println!("Connected to the server!");
             let write_stream = stream.try_write(msg);
-
             match write_stream {
-                Ok(_size) => {}
+                Ok(_size) => {
+                    println!("Sent a message to the server.");
+                }
                 Err(err) => {
                     eprintln!("{}", err)
                 }
