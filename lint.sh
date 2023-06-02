529bc4ab69ef4b9f76bb9f19546823caed3d2ef7
Salmaan Saeed <salsaeed999@gmail.com>
2023-06-02 17:38:03 -0400

    simplify command

--- lint.sh
+++ lint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # lint the project using clippy
-cargo clippy --all-targets --all-features -- -W clippy::all -W clippy::pedantic -W clippy::nursery -W clippy::unwrap_used  -W clippy::missing_docs_in_private_items -W clippy::too_many_lines -W clippy::inefficient_to_string -D warnings
+cargo clippy --all -- -W clippy::all -W clippy::pedantic -W clippy::nursery -W clippy::unwrap_used  -W clippy::missing_docs_in_private_items -W clippy::too_many_lines -W clippy::inefficient_to_string -D warnings
