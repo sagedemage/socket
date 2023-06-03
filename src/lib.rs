029e7801885e33418b480850f4b1f4676fe8f809
Salmaan Saeed <salsaeed999@gmail.com>
2023-06-02 22:58:48 -0400

    linted the lib file

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,20 @@
+//! Message Color Functions
+
+#[must_use]
 pub fn red_message(msg: &str) -> String {
     /* Red message */
     let msg: String = format!("{}{}{}", "\x1b[31m", msg, "\x1b[0m");
     msg
 }
 
+#[must_use]
 pub fn green_message(msg: &str) -> String {
     /* Green message */
     let msg: String = format!("{}{}{}", "\x1b[32m", msg, "\x1b[0m");
     msg
 }
 
+#[must_use]
 pub fn blue_message(msg: &str) -> String {
     /* Blue message */
     let msg: String = format!("{}{}{}", "\x1b[34m", msg, "\x1b[0m");
