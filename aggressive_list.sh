5ac211e804f323932946f1ff10f0ee0a5675ba0f
Salmaan Saeed <salsaeed999@gmail.com>
2023-06-02 20:07:10 -0400

    removed extra script

--- aggressive_list.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# aggresive lint
-#cargo clippy --all -- -W clippy::all -W clippy::pedantic -W clippy::nursery -W clippy::restriction -A clippy::print_stdout -A clippy::print_stderr -A clippy::question_mark_used -A clippy::indexing_slicing -A clippy::std_instead_of_core -D warnings
-
-cargo clippy  --all  -- -W clippy::all -W clippy::pedantic -W clippy::restriction -W clippy::nursery -D warnings
