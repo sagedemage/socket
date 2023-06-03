7a39b2492f49ced2058b1f060ef765e44800fefd
Salmaan Saeed <salsaeed999@gmail.com>
2023-06-02 23:06:03 -0400

    change comments to comment docs

--- src/lib.rs
+++ src/lib.rs
@@ -2,21 +2,21 @@
 
 #[must_use]
 pub fn red_message(msg: &str) -> String {
-    /* Red message */
+    //! Red message
     let msg: String = format!("{}{}{}", "\x1b[31m", msg, "\x1b[0m");
     msg
 }
 
 #[must_use]
 pub fn green_message(msg: &str) -> String {
-    /* Green message */
+    //! Green message
     let msg: String = format!("{}{}{}", "\x1b[32m", msg, "\x1b[0m");
     msg
 }
 
 #[must_use]
 pub fn blue_message(msg: &str) -> String {
-    /* Blue message */
+    //! Blue message
     let msg: String = format!("{}{}{}", "\x1b[34m", msg, "\x1b[0m");
     msg
 }
