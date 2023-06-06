fa63bed5c7c0c18d47c21a39bc0d0ed811690d7b
Salmaan Saeed <salsaeed999@gmail.com>
2023-06-06 02:35:21 -0400

    server prints logo of a network socket

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,35 @@ use std::io;
 use tokio::net::{TcpListener, TcpStream};
 use socket_cli::green_message;
 
+fn print_logo() {
+    let end: &str = "\x1b[0m";
+    let black: &str = "\x1b[30m";
+    let white: &str = "\x1b[37m";
+    let yellow: &str = "\x1b[33m";
+    let green: &str = "\x1b[32m";
+
+    println!("{black}HHHHHHHHHHHHHHHHHHHHH{end}");
+    println!("{black}HH{end}{white}IIIIIIIIIIIIIIIII{end}{black}HH{end}");
+    println!("{black}HH{end}{white}IIIIIIIIIIIIIIIII{end}{black}HH{end}");
+    println!("{black}HH{end}{white}III{end} {yellow}|||||||||{end} {white}III{end}{black}HH{end}");
+    println!("{black}HH{end}{white}III{end}           {white}III{end}{black}HH{end}");
+    println!("{black}HH{end}{white}III{end}           {white}III{end}{black}HH{end}");
+    println!("{black}HH{end}{white}III{end}           {white}III{end}{black}HH{end}");
+    println!("{black}HH{end}{white}IIIII{end}       {white}IIIII{end}{black}HH{end}");
+    println!("{black}HH{end}I{yellow}+++{end}{white}II{end}     {white}II{end}{green}+++{end}I{black}HH{end}");
+    println!("{black}HH{end}{white}IIIIIIIIIIIIIIIII{end}{black}HH{end}");
+    println!("{black}HHHHHHHHHHHHHHHHHHHHH{end}");
+}
+
 #[tokio::main]
 async fn main() -> io::Result<()> {
     // Notify the user that the server is running
     // Using 8-16 Hexadecimal Colors
     let msg1: String = green_message("Server is running!");
     let msg2: String = green_message("------------------");
+    
+    print_logo();
+    println!("");
     println!("{msg1}");
     println!("{msg2}");
 
